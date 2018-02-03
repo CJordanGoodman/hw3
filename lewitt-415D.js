cec931c82cd037e351dce6d2d4e1fb8de6f56d51
CJordanGoodman <good.cjordan@gmail.com>
2018-02-02 19:06:20 -0800

    Create lewitt-415D.js

---
+++ lewitt-415D.js
@@ -0,0 +1,36 @@
+createCanvas(680, 340);
+background("white");
+noStroke();
+fill("#ff5a51");
+rect(50, 150, 150, 150);
+fill("#08c202");
+beginShape();
+vertex(50, 150);
+vertex(200, 150);
+vertex(275, 75);
+vertex(125, 75);
+endShape()
+fill("#5c70de");
+beginShape();
+vertex(200, 150);
+vertex(275, 75);
+vertex(275, 225);
+vertex(200, 300);
+endShape();
+
+fill("#cfcbbf");
+rect(390, 150, 150, 150);
+fill("#f2efe3");
+beginShape();
+vertex(390, 150);
+vertex(540, 150);
+vertex(615, 75);
+vertex(465, 75);
+endShape()
+fill("#a19e94");
+beginShape();
+vertex(540, 150);
+vertex(615, 75);
+vertex(615, 225);
+vertex(540, 300);
+endShape();
